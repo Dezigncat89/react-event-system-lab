db6464888dd080169c77dd84f5f45f10e84337f5
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-19 09:40:34 -0700

    Update EyesOnMe.js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+	handleChange(event){
+		if(event.key === 'f'){
+			console.log('Good!')
+	}
+		else if (event.key === 'b') {
+		  console.log('Hey! Eyes on Me!')
+	}
+}
+
+	return (
+		<div>
+				<h1>Do youy see me?</h1>
+					<input type="text" value={attention} onKeyPress={handleChange}/>
+				<p> Press f for Focus or b for Blur</p>
+	</div>
+)
+}
