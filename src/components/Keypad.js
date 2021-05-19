71aa037d834ac77072af784e4d01d7e93236e8fc
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-19 09:27:30 -0700

    Update Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,33 @@
-// Code Keypad Component Here
+import React, {Component} from "react"
+
+class Keypad extends Component {
+  constructor() {
+    super()
+    this.state = {
+      thePassword: ""
+   }
+   this.onKeyUp = this.onKeyUp.bind(this)
+}
+
+onKeyUp(event) {
+  if (event.key === 'Enter'){
+    console.log('Entering Password...)
+   }
+} 
+
+
+
+  render() {
+    return (
+      <form>
+        <h1>Enter Password Now<h1>
+        <input type="password" value={this.state.thePassword} 
+       name="thePassword" placeholder="Enter Password" onKeyPress={this.onKeyUp} />
+       <p> Hit Enter to Continue<p>
+      </form>
+      )
+    }
+}
+
+
+export default Keypad
